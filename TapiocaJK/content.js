f8baac9566e9d7e477d2d456672a8e41f1ee147f
kojingharang <kojingharang@gmail.com>
2022-09-22 22:04:04 +0900

    fix going back bug on 2nd page

--- TapiocaJK/content.js
+++ TapiocaJK/content.js
@@ -4,18 +4,18 @@ var index = 0;
 
 function move(delta) {
 	if(links.length==0) return;
-//	console.log("index", index, "delta", delta, "links", links, "goBack", goBack, "goNext", goNext);
+//	console.log("move", "index", index, "delta", delta, "links", links, "goBack", goBack, "goNext", goNext);
 
-	if(index==0 && goBack) {
+	index += delta;
+	if(index==-1 && goBack) {
 		window.location.href = goBack;
 		return;
 	}
-	if(index==links.length-1 && goNext) {
+	if(index==links.length && goNext) {
 		window.location.href = goNext;
 		return;
 	}
 
-	index += delta;
 	index = Math.min(Math.max(index, 0), links.length-1);
 	if(links[index]) {
 		links[index].focus();
@@ -29,9 +29,9 @@ $(function() {
 	links = $("div.g div.yuRUbf > a:not([class])");
 	goBack = $("a#pnprev").attr("href");
 	goNext = $("a#pnnext").attr("href");
-	//console.log("links", links)
-	//console.log("back", goBack);
-	//console.log("next", goNext);
+//	console.log("links", links)
+//	console.log("back", goBack);
+//	console.log("next", goNext);
 
 	$(window).keydown(function(e) {
 //		console.log("keyDown", e.keyCode, "focus", $(":focus").prop("tagName"));
